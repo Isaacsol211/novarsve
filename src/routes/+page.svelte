7808883712ae22e533311bec7632be02e07f0a1d
isaac solomon <49633972+IsaacSolomon@users.noreply.github.com>
2024-07-06 23:46:10 +0530

    coming soon page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>Nova Rock Coming Soon...</title> 
+</svelte:head>
+<div class="w-screen h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+  <div class="flex flex-col gap-10 items-center bg-black bg-opacity-50 py-20 px-10 rounded-3xl w-10/12 shadow-2xl relative max-w-[900px]">
+    <span class="loader absolute -left-10 -top-16"></span>
+    <div class="text-white text-center flex flex-col gap-4">
+      <p class="title text-2xl uppercase">Coming Soon</p>
+      <p class="text-sm leading-6">Our site under construction. For any queries, please contact <a href="mailto:info@novarocktools.com" class="underline block">info@novarocktools.com</a></p>
+    </div>
+  </div>
+</div>
+<style>
+  
+   .title {
+    font-variation-settings: 'wght' 900;
+   }   
+</style>
