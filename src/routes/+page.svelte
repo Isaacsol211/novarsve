1117d73ddf407aa6968eda414d3299f48148483a
isaac solomon <49633972+IsaacSolomon@users.noreply.github.com>
2024-07-08 17:00:56 +0530

    ui fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <svelte:head>
     <title>Nova Rock Coming Soon...</title> 
 </svelte:head>
-<div class="w-screen h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+<div class="w-screen h-screen flex items-center justify-center bg-gradient-to-r from-[#315187] to-[#071a3a]">
+  <div class="bg-white p-10 absolute top-0 left-0 right-0">
+    <img src="/images/logo.svg" alt="" class="absolute left-1/2 transform -translate-x-1/2 xl:transform-none xl:left-auto xl:right-5 top-0 w-40 aspect-video object-contain">
+  </div>
   <div class="flex flex-col gap-10 items-center bg-black bg-opacity-50 py-20 px-10 rounded-3xl w-10/12 shadow-2xl relative max-w-[900px]">
     <span class="loader absolute -left-10 -top-16"></span>
     <div class="text-white text-center flex flex-col gap-4">
